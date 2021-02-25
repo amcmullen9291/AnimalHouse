4341c8b5863750363e92e27730d44dcfe30f7966
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-25 09:36:03 -0500

    added staff page

--- app/controllers/the_eleanor_grigsby_families_controller.rb
+++ app/controllers/the_eleanor_grigsby_families_controller.rb
@@ -1,8 +1,15 @@
 class TheEleanorGrigsbyFamiliesController < ApplicationController
 
+    def new
+    end 
+
+    def create 
+    end 
+
     def index 
     end
 
     def show 
     end 
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_033929) do
+ActiveRecord::Schema.define(version: 2021_02_25_140444) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_033929) do
     t.integer "appointment_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
