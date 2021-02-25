034bbe4c42aeb6867476de3b911001f01a256d9b
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-24 23:49:20 -0500

    fixing routes

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,39 @@
 class OwnersController < ApplicationController
+    def new 
+        @owner = Owner.new
+    end 
+
+    def create 
+        @owner = Owner.new(owner_params)
+        if @owner.save 
+            redirect_to the_eleanor_grigsby_families_path
+        else
+            render :new
+        end 
+    end 
+
+    def index 
+    end 
+
+    def show 
+    end 
+
+    def edit 
+    end
+
+    def update 
+    end 
+
+    def destroy
+    end 
+
+    private 
+
+    def owner_params 
+        params.require(:owner).permit(:name, :email, :email_confirmation, :password, :password_digest, :password_confirmation, :notes, :telephone, :other_dogs)
+    end 
+
+    def set_owner 
+        @owner = Owner.find_by(params[:id])
+    end
 end
