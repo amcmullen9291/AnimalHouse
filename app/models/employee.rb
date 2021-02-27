10c02516fd8cbb2841ac04e14fb456ba0901c0c0
amcmullen9291 <amcmullen9291@gmail.com>
2021-02-26 19:50:00 -0500

    working on rails-has-many-through-with-collection-select-form-helper

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,9 +1,10 @@
 class Employee < ApplicationRecord
-    extend ActiveModel::Naming
     serialize :breed, Array
     has_and_belongs_to_many :appointments 
+    has_many :breeds, through: :appointments
     validates :bio, length: { maximum: 1000,
     too_long: "%{count} characters is the maximum allowed" }
     has_one_attached :avatar
+    accepts_nested_attributes_for :breed_id
     
 end
